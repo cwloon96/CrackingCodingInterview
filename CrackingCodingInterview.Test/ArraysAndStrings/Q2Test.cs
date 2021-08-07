fc6827c19627aff6377e32f5875a3791c8e73f17
Woei Loon <woeiloon96@gmail.com>
2021-08-07 09:24:35 +0800

    Check Permutation

---
+++ CrackingCodingInterview.Test/ArraysAndStrings/Q2Test.cs
@@ -0,0 +1,33 @@
+﻿using CrackingCodingInterview.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.ArraysAndStrings
+{
+    [TestClass]
+    public class Q2Test
+    {
+        [TestMethod]
+        public void S1ShouldBePermuted()
+        {
+            Assert.AreEqual(true, new Q2().CheckPermutationS1("abc ", "c ba"));
+        }
+
+        [TestMethod]
+        public void S1ShouldBeNotPermuted()
+        {
+            Assert.AreEqual(false, new Q2().CheckPermutationS1("abc", "abz"));
+        }
+
+        [TestMethod]
+        public void S2ShouldBePermuted()
+        {
+            Assert.AreEqual(true, new Q2().CheckPermutationS2("abc ", "c ba"));
+        }
+
+        [TestMethod]
+        public void S2ShouldBeNotPermuted()
+        {
+            Assert.AreEqual(false, new Q2().CheckPermutationS2("abc", "abz"));
+        }
+    }
+}
---
+++ CrackingCodingInterview/ArraysAndStrings/Q2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CrackingCodingInterview.ArraysAndStrings
+{
+    public class Q2
+    {
+        public bool CheckPermutationS1(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+
+            var sortedS1 = GetSortedArr(s1);
+            var sortedS2 = GetSortedArr(s2);
+
+            for (int i = 0; i < sortedS1.Length; i++)
+            {
+                if (sortedS1[i] != sortedS2[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        private char[] GetSortedArr(string s)
+        {
+            var arr = s.ToCharArray();
+            Array.Sort(arr);
+            return arr;
+        }
+
+        // works for ASCII only as arr fixed to 128
+        public bool CheckPermutationS2(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+
+            var arr = new int[128];
+
+            for (int i = 0; i < s1.Length; i++)
+                arr[s1[i]]++;
+            
+            for (int i = 0; i < s2.Length; i++)
+            {
+                arr[s2[i]]--;
+
+                // both string mush be same size, thus extra character must be < 0
+                // eg: s1 = abc, s2 = abz, z will be < 0 in this case
+                if (arr[s2[i]] < 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
