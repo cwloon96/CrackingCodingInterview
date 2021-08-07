e3767fa462a19cde727afcaf8b82499d49adeccf
Woei Loon <woeiloon96@gmail.com>
2021-08-07 12:32:45 +0800

    Urlify

---
+++ CrackingCodingInterview.Test/ArraysAndStrings/Q3Test.cs
@@ -0,0 +1,20 @@
+﻿using CrackingCodingInterview.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.ArraysAndStrings
+{
+    [TestClass]
+    public class Q3Test
+    {
+        [TestMethod]
+        public void S1ShouldBeUrlified()
+        {
+            string s = "Mr John Smith      ";
+            var arr = s.ToCharArray();
+            new Q3().UrlifyS1(arr, 13);
+
+            string expected = "Mr%20John%20Smith\0 ";
+            Assert.AreEqual(expected, new string(arr));
+        }
+    }
+}
---
+++ CrackingCodingInterview/ArraysAndStrings/Q3.cs
@@ -0,0 +1,30 @@
+﻿namespace CrackingCodingInterview.ArraysAndStrings
+{
+    public class Q3
+    {
+        public void UrlifyS1(char[] s, int trueLength)
+        {
+            var spaceCount = 0;
+            for (int i = 0; i < trueLength; i++)
+                if (s[i] == ' ')
+                    spaceCount++;
+
+            var newTrueLength = trueLength - spaceCount + (spaceCount * 3);
+            s[newTrueLength] = '\0';
+
+            for (int left = trueLength - 1, right = newTrueLength - 1; left >= 0; left--)
+            {
+                if (s[left] == ' ')
+                {
+                    s[right--] = '0';
+                    s[right--] = '2';
+                    s[right--] = '%';
+                }
+                else
+                {
+                    s[right--] = s[left];
+                }
+            }
+        }
+    }
+}
