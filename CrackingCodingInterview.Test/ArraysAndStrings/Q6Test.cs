c2c62c0a5d01300c3cb025f0b431c9b5c16ce99c
Woei Loon <woeiloon96@gmail.com>
2021-08-08 16:40:13 +0800

    String Compression

---
+++ CrackingCodingInterview.Test/ArraysAndStrings/Q6Test.cs
@@ -0,0 +1,45 @@
+﻿using CrackingCodingInterview.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.ArraysAndStrings
+{
+    [TestClass]
+    public class Q6Test
+    {
+        [TestMethod]
+        public void S1ShouldBeCompressed1()
+        {
+            Assert.AreEqual("a2b1c5a3", new Q6().StringCompressionS1("aabcccccaaa"));
+        }
+
+        [TestMethod]
+        public void S1ShouldBeCompressed2()
+        {
+            Assert.AreEqual("a2b1c5a1", new Q6().StringCompressionS1("aabccccca"));
+        }
+
+        [TestMethod]
+        public void S1ShouldNotBeCompressed1()
+        {
+            Assert.AreEqual("abcd", new Q6().StringCompressionS1("abcd"));
+        }
+
+        [TestMethod]
+        public void S2ShouldBeCompressed1()
+        {
+            Assert.AreEqual("a2b1c5a3", new Q6().StringCompressionS2("aabcccccaaa"));
+        }
+
+        [TestMethod]
+        public void S2ShouldBeCompressed2()
+        {
+            Assert.AreEqual("a2b1c5a1", new Q6().StringCompressionS2("aabccccca"));
+        }
+
+        [TestMethod]
+        public void S2ShouldNotBeCompressed1()
+        {
+            Assert.AreEqual("abcd", new Q6().StringCompressionS2("abcd"));
+        }
+    }
+}
---
+++ CrackingCodingInterview/ArraysAndStrings/Q6.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+
+namespace CrackingCodingInterview.ArraysAndStrings
+{
+    public class Q6
+    {
+        public string StringCompressionS1(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                count++;
+
+                if (i + 1 >= s.Length || s[i] != s[i + 1])
+                {
+                    sb.Append(s[i]);
+                    sb.Append(count);
+                    count = 0;
+                }
+            }
+
+            if (sb.Length < s.Length)
+                return sb.ToString();
+
+            return s;
+        }
+
+        public string StringCompressionS2(string s)
+        {
+            int finalLength = CalculateCompressedLength(s);
+            if (finalLength >= s.Length)
+                return s;
+
+            StringBuilder sb = new StringBuilder(finalLength);
+
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                count++;
+
+                if (i + 1 >= s.Length || s[i] != s[i + 1])
+                {
+                    sb.Append(s[i]);
+                    sb.Append(count);
+                    count = 0;
+                }
+            }
+
+            if (sb.Length < s.Length)
+                return sb.ToString();
+
+            return s;
+        }
+
+        private int CalculateCompressedLength(string s)
+        {
+            int length = 0;
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                count++;
+
+                if (i + 1 >= s.Length || s[i] != s[i + 1])
+                {
+                    // 1 + count length = character + repeat count length
+                    length += 1 + GetIntLength(count);
+                    count = 0;
+                }
+            }
+
+            return length;
+        }
+
+        private int GetIntLength(int value)
+        {
+            // eg: log(10)10 = 1 + 1 = 2
+            // eg: log(10)100 = 2 + 1 = 3
+            return (int)Math.Floor(Math.Log10(value)) + 1;
+        }
+    }
+}
