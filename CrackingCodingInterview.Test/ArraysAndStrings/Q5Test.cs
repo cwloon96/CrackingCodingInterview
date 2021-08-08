0aa38def30feacfb9f31a7eca55d25f3ef521bf1
Woei Loon <woeiloon96@gmail.com>
2021-08-08 11:29:58 +0800

    One Away

---
+++ CrackingCodingInterview.Test/ArraysAndStrings/Q5Test.cs
@@ -0,0 +1,45 @@
+﻿using CrackingCodingInterview.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.ArraysAndStrings
+{
+    [TestClass]
+    public class Q5Test
+    {
+        [TestMethod]
+        public void S1ShouldBeTrueWithOneModification()
+        {
+            Assert.AreEqual(true, new Q5().OneAwayS1("pale", "pall"));
+        }
+
+        [TestMethod]
+        public void S1ShouldBeTrueWithOneDeletion()
+        {
+            Assert.AreEqual(true, new Q5().OneAwayS1("pale", "ple"));
+        }
+
+        [TestMethod]
+        public void S1ShouldBeTrueWithOneInsertion()
+        {
+            Assert.AreEqual(true, new Q5().OneAwayS1("pale", "palle"));
+        }
+
+        [TestMethod]
+        public void S1ShouldBeFalseWithTwoModification()
+        {
+            Assert.AreEqual(false, new Q5().OneAwayS1("pale", "bake"));
+        }
+
+        [TestMethod]
+        public void S1ShouldBeFalseWithTwoDeletion()
+        {
+            Assert.AreEqual(false, new Q5().OneAwayS1("pale", "pe"));
+        }
+
+        [TestMethod]
+        public void S1ShouldBeFalseWithTwoInsertion()
+        {
+            Assert.AreEqual(false, new Q5().OneAwayS1("pe", "pale"));
+        }
+    }
+}
---
+++ CrackingCodingInterview/ArraysAndStrings/Q5.cs
@@ -0,0 +1,31 @@
+﻿namespace CrackingCodingInterview.ArraysAndStrings
+{
+    public class Q5
+    {
+        public bool OneAwayS1(string s1, string s2)
+        {
+            if (s1.Length > s2.Length)
+                return OneAwayS1(s2, s1);
+
+            if (s2.Length - s1.Length > 1)
+                return false;
+
+            bool changed = false;
+            for (int i = 0, j = 0; i < s1.Length; i++, j++)
+            {
+                if (s1[i] != s2[j])
+                {
+                    if (changed)
+                        return false;
+
+                    changed = true;
+
+                    if (s1.Length != s2.Length)
+                        j++;
+                }
+            }
+
+            return true;
+        }
+    }
+}
