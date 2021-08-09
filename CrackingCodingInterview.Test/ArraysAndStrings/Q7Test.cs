0dffe5b7cb562c237ede8d3a6615cd3685f009bf
Woei Loon <woeiloon96@gmail.com>
2021-08-09 08:26:24 +0800

    Rotate Matrix

---
+++ CrackingCodingInterview.Test/ArraysAndStrings/Q7Test.cs
@@ -0,0 +1,32 @@
+﻿using CrackingCodingInterview.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.ArraysAndStrings
+{
+    [TestClass]
+    public class Q7Test
+    {
+        [TestMethod]
+        public void S1ShouldBeRotated()
+        {
+            var matrix = new int[4, 4]
+            {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 },
+            };
+            new Q7().RotateMatrixS1(matrix);
+
+            var expected = new int[4, 4]
+            {
+                { 13, 9, 5, 1 },
+                { 14, 10, 6, 2 },
+                { 15, 11, 7, 3 },
+                { 16, 12, 8, 4 },
+            };
+
+            CollectionAssert.AreEqual(expected, matrix);
+        }
+    }
+}
---
+++ CrackingCodingInterview/ArraysAndStrings/Q7.cs
@@ -0,0 +1,39 @@
+﻿namespace CrackingCodingInterview.ArraysAndStrings
+{
+    public class Q7
+    {
+        public void RotateMatrixS1(int[,] matrix)
+        {
+            if (matrix.Length == 0 || matrix.GetLength(0) != matrix.GetLength(1)) 
+                return;
+
+            // 4 X 4 -> 2 layer, 6 X 6 -> 3 layer
+            for (int layer = 0; layer < matrix.GetLength(0) / 2; layer++)
+            {
+                // first element of the layer
+                int first = layer;
+                // last element of the layer
+                int last = matrix.GetLength(0) - 1 - layer;
+
+                for (int i = first; i < last; i++)
+                {
+                    int offset = i - first;
+
+                    int top = matrix[first, i];
+
+                    // left to top
+                    matrix[first, i] = matrix[last - offset, first];
+
+                    // bottom to left
+                    matrix[last - offset, first] = matrix[last, last - offset];
+
+                    // right to bottom
+                    matrix[last, last - offset] = matrix[i, last];
+
+                    // top to right
+                    matrix[i, last] = top;
+                }
+            }
+        }
+    }
+}
