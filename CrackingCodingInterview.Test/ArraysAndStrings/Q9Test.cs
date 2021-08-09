703559740e11aafda3c8d5f902dd3591809730cb
Woei Loon <woeiloon96@gmail.com>
2021-08-09 16:41:15 +0800

    string rotation

---
+++ CrackingCodingInterview.Test/ArraysAndStrings/Q9Test.cs
@@ -0,0 +1,15 @@
+﻿using CrackingCodingInterview.ArraysAndStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.ArraysAndStrings
+{
+    [TestClass]
+    public class Q9Test
+    {
+        [TestMethod]
+        public void S1ShouldBeTrue()
+        {
+            Assert.AreEqual(true, new Q9().StringRotationS1("waterbottle", "erbottlewat"));
+        }
+    }
+}
---
+++ CrackingCodingInterview/ArraysAndStrings/Q9.cs
@@ -0,0 +1,24 @@
+﻿namespace CrackingCodingInterview.ArraysAndStrings
+{
+    public class Q9
+    {
+        public bool StringRotationS1(string s1, string s2)
+        {
+            if (s1.Length == s2.Length)
+            {
+                var s1s1 = s1 + s1;
+
+                for (int i = 0; i < s1s1.Length; i ++)
+                {
+                    if (s1s1[i] == s2[0])
+                    {
+                        if (s1s1.Substring(i, s2.Length) == s2)
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
