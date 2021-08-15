e9ae12c89c131ca2d8d40623b38d0ca06ec06dfa
Woei Loon <woeiloon96@gmail.com>
2021-08-15 18:05:27 +0800

    Delete middle node

---
+++ CrackingCodingInterview.Test/LinkedLists/Q3Test.cs
@@ -0,0 +1,23 @@
+﻿using CrackingCodingInterview.LinkedLists;
+using CrackingCodingInterview.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.LinkedLists
+{
+    [TestClass]
+    public class Q3Test
+    {
+        [TestMethod]
+        public void S1ShouldRemoveNode()
+        {
+            var nodeToRemove = new ListNode<int>(2, new ListNode<int>(3, new ListNode<int>(4)));
+            var listNode = new ListNode<int>(1, nodeToRemove);
+            
+            new Q3().DeleteMiddleNodeS1(nodeToRemove);
+
+            Assert.AreEqual(1, listNode.Value);
+            Assert.AreEqual(3, listNode.Next.Value);
+            Assert.AreEqual(4, listNode.Next.Next.Value);
+        }
+    }
+}
---
+++ CrackingCodingInterview/LinkedLists/Q3.cs
@@ -0,0 +1,16 @@
+﻿using CrackingCodingInterview.Model;
+
+namespace CrackingCodingInterview.LinkedLists
+{
+    public class Q3
+    {
+        public void DeleteMiddleNodeS1(ListNode<int> node)
+        {
+            if (node.Next != null)
+            {
+                node.Value = node.Next.Value;
+                node.Next = node.Next.Next;
+            }
+        }
+    }
+}
