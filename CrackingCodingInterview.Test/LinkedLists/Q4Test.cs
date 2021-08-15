15e26aea3ff4e3fbb12648dcb2c4cef0ba683274
Woei Loon <woeiloon96@gmail.com>
2021-08-15 19:24:54 +0800

    Partition

---
+++ CrackingCodingInterview.Test/LinkedLists/Q4Test.cs
@@ -0,0 +1,41 @@
+﻿using CrackingCodingInterview.LinkedLists;
+using CrackingCodingInterview.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.LinkedLists
+{
+    [TestClass]
+    public class Q4Test
+    {
+        [TestMethod]
+        public void S1ShouldBePartitioned()
+        {
+            var listNode = new ListNode<int>(6, new ListNode<int>(5, new ListNode<int>(1, new ListNode<int>(2, new ListNode<int>(4, new ListNode<int>(7))))));
+            
+            var result = new Q4().PartitionS1(listNode, 5);
+
+            Assert.AreEqual(1, result.Value);
+            Assert.AreEqual(2, result.Next.Value);
+            Assert.AreEqual(4, result.Next.Next.Value);
+            Assert.AreEqual(6, result.Next.Next.Next.Value);
+            Assert.AreEqual(5, result.Next.Next.Next.Next.Value);
+            Assert.AreEqual(7, result.Next.Next.Next.Next.Next.Value);
+        }
+
+        [TestMethod]
+        public void S2ShouldBePartitioned()
+        {
+            var listNode = new ListNode<int>(6, new ListNode<int>(5, new ListNode<int>(1, new ListNode<int>(2, new ListNode<int>(4, new ListNode<int>(7, new ListNode<int>(3)))))));
+
+            var result = new Q4().PartitionS2(listNode, 5);
+
+            Assert.AreEqual(3, result.Value);
+            Assert.AreEqual(4, result.Next.Value);
+            Assert.AreEqual(2, result.Next.Next.Value);
+            Assert.AreEqual(1, result.Next.Next.Next.Value);
+            Assert.AreEqual(6, result.Next.Next.Next.Next.Value);
+            Assert.AreEqual(5, result.Next.Next.Next.Next.Next.Value);
+            Assert.AreEqual(7, result.Next.Next.Next.Next.Next.Next.Value);
+        }
+    }
+}
---
+++ CrackingCodingInterview/LinkedLists/Q4.cs
@@ -0,0 +1,77 @@
+﻿using CrackingCodingInterview.Model;
+
+namespace CrackingCodingInterview.LinkedLists
+{
+    public class Q4
+    {
+        public ListNode<int> PartitionS1(ListNode<int> node, int partition)
+        {
+            ListNode<int> lessThan = null; // keep track nodes < partition
+            ListNode<int> afterLess = null; // keep track last node in lessThan
+
+            ListNode<int> moreThan = null; // keep track nodes >= partition
+            ListNode<int> afterMore = null; // keep track last node in moreThan
+
+            while (node != null)
+            {
+                var next = node.Next;
+                node.Next = null;
+
+                if (node.Value < partition)
+                {
+                    if (lessThan == null)
+                        lessThan = node;
+                    else
+                        afterLess.Next = node;
+
+                    afterLess = node;
+                }
+                else
+                {
+                    if (moreThan == null)
+                        moreThan = node;
+                    else
+                        afterMore.Next = node;
+
+                    afterMore = node;
+                }
+
+                node = next;
+            }
+
+            if (lessThan == null)
+                return moreThan;
+
+            afterLess.Next = moreThan;
+            return lessThan;
+        }
+
+        public ListNode<int> PartitionS2(ListNode<int> node, int partition)
+        {
+            var head = node;
+            var tail = node;
+
+            while (node != null)
+            {
+                var next = node.Next;
+
+                if (node.Value < partition)
+                {
+                    node.Next = head;
+                    head = node;
+                }
+                else
+                {
+                    tail.Next = node;
+                    tail = node;    
+                }
+
+                node = next;
+            }
+
+            tail.Next = null;
+
+            return head;
+        }
+    }
+}
