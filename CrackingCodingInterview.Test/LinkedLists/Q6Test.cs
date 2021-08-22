fd351c3b537d0a7ee8c867240896223d3f0450e5
Woei Loon <woeiloon96@gmail.com>
2021-08-22 20:24:36 +0800

    Palindrome

---
+++ CrackingCodingInterview.Test/LinkedLists/Q6Test.cs
@@ -0,0 +1,58 @@
+﻿using CrackingCodingInterview.LinkedLists;
+using CrackingCodingInterview.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.LinkedLists
+{
+    [TestClass]
+    public class Q6Test
+    {
+        [TestMethod]
+        public void S1ShouldBePalindromeWithEvenLength()
+        {
+            var listNode = new ListNode<char>('a', new ListNode<char>('b', new ListNode<char>('b', new ListNode<char>('a'))));
+
+            Assert.AreEqual(true, new Q6().IsPalindromeS1(listNode));
+        }
+
+        [TestMethod]
+        public void S1ShouldBePalindromeWithOddLength()
+        {
+            var listNode = new ListNode<char>('a', new ListNode<char>('b', new ListNode<char>('c', new ListNode<char>('b', new ListNode<char>('a')))));
+
+            Assert.AreEqual(true, new Q6().IsPalindromeS1(listNode));
+        }
+
+        [TestMethod]
+        public void S1ShouldNotBePalindrome()
+        {
+            var listNode = new ListNode<char>('a', new ListNode<char>('b', new ListNode<char>('b', new ListNode<char>('a', new ListNode<char>('c')))));
+
+            Assert.AreEqual(false, new Q6().IsPalindromeS1(listNode));
+        }
+
+        [TestMethod]
+        public void S2ShouldBePalindromeWithEvenLength()
+        {
+            var listNode = new ListNode<char>('a', new ListNode<char>('b', new ListNode<char>('b', new ListNode<char>('a'))));
+
+            Assert.AreEqual(true, new Q6().IsPalindromeS1(listNode));
+        }
+
+        [TestMethod]
+        public void S2ShouldBePalindromeWithOddLength()
+        {
+            var listNode = new ListNode<char>('a', new ListNode<char>('b', new ListNode<char>('c', new ListNode<char>('b', new ListNode<char>('a')))));
+
+            Assert.AreEqual(true, new Q6().IsPalindromeS1(listNode));
+        }
+
+        [TestMethod]
+        public void S2ShouldNotBePalindrome()
+        {
+            var listNode = new ListNode<char>('a', new ListNode<char>('b', new ListNode<char>('b', new ListNode<char>('a', new ListNode<char>('c')))));
+
+            Assert.AreEqual(false, new Q6().IsPalindromeS1(listNode));
+        }
+    }
+}
---
+++ CrackingCodingInterview/LinkedLists/Q6.cs
@@ -0,0 +1,68 @@
+﻿using CrackingCodingInterview.Model;
+using System.Collections.Generic;
+
+namespace CrackingCodingInterview.LinkedLists
+{
+    public class Q6
+    {
+        public bool IsPalindromeS1(ListNode<char> node)
+        {
+            var stack = new Stack<char>();
+
+            ListNode<char> slow = node;
+            ListNode<char> fast = node;
+
+            while (fast != null && fast.Next != null)
+            {
+                stack.Push(slow.Value);
+
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            // if length is odd, fast will be last element & slow will be at middle element
+            if (fast != null)
+                slow = slow.Next;
+
+            while (slow != null)
+            {
+                if (stack.Pop() != slow.Value)
+                    return false;
+
+                slow = slow.Next;
+            }
+
+            return true;
+        }
+
+        public bool IsPalindromeS2(ListNode<char> node)
+        {
+            var reversedNode = CloneReversed(node);
+
+            while (node != null)
+            {
+                if (node.Value != reversedNode.Value)
+                    return false;
+
+                node = node.Next;
+                reversedNode = reversedNode.Next;
+            }
+
+            return true;
+        }
+
+        public ListNode<char> CloneReversed(ListNode<char> node)
+        {
+            ListNode<char> head = null;
+
+            while (node != null)
+            {
+                var newNode = new ListNode<char>(node.Value, head);
+                head = newNode;
+                node = node.Next;
+            }
+
+            return head;
+        }
+    }
+}
