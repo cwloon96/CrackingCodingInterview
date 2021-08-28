ee4925ac54a543bcbc66cb7e65296348ba8ea9fd
Woei Loon <woeiloon96@gmail.com>
2021-08-28 20:42:49 +0800

    Intersection

---
+++ CrackingCodingInterview.Test/LinkedLists/Q7Test.cs
@@ -0,0 +1,29 @@
+﻿using CrackingCodingInterview.LinkedLists;
+using CrackingCodingInterview.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.LinkedLists
+{
+    [TestClass]
+    public class Q7Test
+    {
+        [TestMethod]
+        public void S1IntersectionShouldBeFound()
+        {
+            var intersect = new ListNode<int>(7, new ListNode<int>(2, new ListNode<int>(1)));
+            var l1 = new ListNode<int>(3, new ListNode<int>(1, new ListNode<int>(5, new ListNode<int>(9, intersect))));
+            var l2 = new ListNode<int>(4, new ListNode<int>(6, intersect));
+
+            Assert.AreEqual(intersect, new Q7().FindIntersectionS1(l1, l2));
+        }
+
+        [TestMethod]
+        public void S1IntersectionShouldBeNull()
+        {
+            var l1 = new ListNode<int>(3, new ListNode<int>(1, new ListNode<int>(5, new ListNode<int>(9, new ListNode<int>(7, new ListNode<int>(2, new ListNode<int>(1)))))));
+            var l2 = new ListNode<int>(4, new ListNode<int>(6, new ListNode<int>(7, new ListNode<int>(2, new ListNode<int>(1)))));
+
+            Assert.AreEqual(null, new Q7().FindIntersectionS1(l1, l2));
+        }
+    }
+}
---
+++ CrackingCodingInterview/LinkedLists/Q7.cs
@@ -0,0 +1,55 @@
+﻿using CrackingCodingInterview.Model;
+
+namespace CrackingCodingInterview.LinkedLists
+{
+    public class Q7
+    {
+        public ListNode<int> FindIntersectionS1(ListNode<int> l1, ListNode<int> l2)
+        {
+            var result1 = GetLastNodeAndLength(l1);
+            var result2 = GetLastNodeAndLength(l2);
+
+            if (result1.Item1 != result2.Item1)
+                return null;
+
+            if (result1.Item2 > result2.Item2)
+            {
+                l1 = MoveForward(l1, result1.Item2 - result2.Item2);
+            }
+            else if (result2.Item2 > result1.Item2)
+            {
+                l2 = MoveForward(l2, result2.Item2 - result1.Item2);
+            }
+
+            while (l1 != l2)
+            {
+                l1 = l1.Next;
+                l2 = l2.Next;
+            }
+
+            return l1;
+        }
+
+        private (ListNode<int>, int) GetLastNodeAndLength(ListNode<int> node)
+        {
+            int length = 0;
+            var temp = node;
+
+            while (temp.Next != null)
+            {
+                length++;
+                temp = temp.Next;
+            }
+
+            return (temp, length);
+        }
+
+        private ListNode<int> MoveForward(ListNode<int> node, int steps)
+        {
+            for (int i = steps; i > 0; i--)
+                node = node.Next;
+
+            return node;
+        }
+    }
+}
