f830662cab88ca8ac6eb22d263336b94b382da0c
Woei Loon <woeiloon96@gmail.com>
2021-08-29 12:37:29 +0800

    Loop Detection:

---
+++ CrackingCodingInterview.Test/LinkedLists/Q8Test.cs
@@ -0,0 +1,30 @@
+﻿using CrackingCodingInterview.LinkedLists;
+using CrackingCodingInterview.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCodingInterview.Test.LinkedLists
+{
+    [TestClass]
+    public class Q8Test
+    {
+        [TestMethod]
+        public void S1LoopBeginShouldBeFound()
+        {
+            var loopBegin = new ListNode<int>(3);
+            var loopList = new ListNode<int>(4, new ListNode<int>(5, loopBegin));
+            loopBegin.Next = loopList;
+
+            var node = new ListNode<int>(1, new ListNode<int>(2, loopBegin));
+
+            Assert.AreEqual(loopBegin, new Q8().FindLoopBeginningS1(node));
+        }
+
+        [TestMethod]
+        public void S1LoopBeginShouldBeNull()
+        {
+            var node = new ListNode<int>(1, new ListNode<int>(2, new ListNode<int>(3, new ListNode<int>(4, new ListNode<int>(5)))));
+
+            Assert.AreEqual(null, new Q8().FindLoopBeginningS1(node));
+        }
+    }
+}
---
+++ CrackingCodingInterview/LinkedLists/Q8.cs
@@ -0,0 +1,36 @@
+﻿using CrackingCodingInterview.Model;
+
+namespace CrackingCodingInterview.LinkedLists
+{
+    public class Q8
+    {
+        public ListNode<int> FindLoopBeginningS1(ListNode<int> node)
+        {
+            var fast = node;
+            var slow = node;
+
+            while (fast != null && fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+
+                if (slow == fast)
+                    break;
+            }
+
+            // no loop found
+            if (fast == null || fast.Next == null)
+                return null;
+
+            slow = node;
+
+            while (slow != fast)
+            {
+                slow = slow.Next;
+                fast = fast.Next;
+            }
+
+            return fast;
+        }
+    }
+}
